26da2c63ffff0ab459b75543cf469573d40c37dc
Rishabh Shairy <rishusinha8@gmail.com>
2019-12-12 14:33:35 +0000

    [LeetCode Sync] Runtime - 44 ms (100.00%), Memory - 12.1 MB (99.95%)

---
+++ my-folder/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start, end = 0, len(numbers)-1
+        while start < end:
+            if numbers[start] + numbers[end] == target:
+                return [start + 1, end + 1]
+            elif numbers[start] + numbers[end] > target:
+                end -= 1
+            else:
+                start += 1
+
+        
