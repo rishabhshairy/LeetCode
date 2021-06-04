7f65f5c092a285868321c3bc445db8c3cf07c72d
Rishabh Shairy <rishusinha8@gmail.com>
2021-06-04 20:12:35 +0000

    [LeetCode Sync] Runtime - 4 ms (48.51%), Memory - 53 MB (6.84%)

--- my-folder/1019-squares-of-a-sorted-array/solution.java
+++ my-folder/1019-squares-of-a-sorted-array/solution.java
@@ -1,19 +1,9 @@
 class Solution {
     public int[] sortedSquares(int[] A) {
-       int leftIndex = 0;
-		int rightIndex = A.length - 1;
-		int[] sqauredSorted = new int[A.length];
-
-		for (int i = 0; i < A.length; i++) {
-			if (Math.abs(A[leftIndex]) > Math.abs(A[rightIndex])) {
-				sqauredSorted[A.length - 1 - i] = A[leftIndex] * A[leftIndex];
-				leftIndex++;
-			} else {
-				sqauredSorted[A.length - 1 - i] = A[rightIndex] * A[rightIndex];
-				rightIndex--;
-			}
-
-		}
-		return sqauredSorted;
+        for(int i=0;i<A.length;i++){
+            A[i]=A[i]*A[i];
+        }
+        Arrays.sort(A);
+        return A;
     }
 }
