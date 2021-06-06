91e9373f648a6ef6033cf164432b8878060b0486
Rishabh Shairy <rishusinha8@gmail.com>
2021-06-06 18:57:06 +0000

    [LeetCode Sync] Runtime - 2 ms (85.28%), Memory - 38.5 MB (100.00%)

---
+++ my-folder/1137-height-checker/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] exp = Arrays.copyOf(heights,heights.length);
+        Arrays.sort(exp);
+        int count=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=exp[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
