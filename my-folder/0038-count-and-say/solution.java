19eb0ad1629ec8ce93b51e0ae103e6b859bfea4b
Rishabh Shairy <rishusinha8@gmail.com>
2021-06-13 15:41:29 +0000

    [LeetCode Sync] Runtime - 5 ms (50.87%), Memory - 36.6 MB (100.00%)

---
+++ my-folder/0038-count-and-say/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        if (n == 2) {
+            return "11";
+        }
+        String s = "11";
+        for (int i = 3; i <= n; i++) {
+            s = s + "&";
+            StringBuilder temp = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < s.length(); j++) {
+                if (s.charAt(j - 1) == s.charAt(j)) {
+                    count++;
+                } else {
+                    temp.append(count);
+                    temp.append(s.charAt(j - 1));
+                    count = 1;
+                }
+            }
+            s = temp.toString();
+        }
+        return s;
+    }
+}
