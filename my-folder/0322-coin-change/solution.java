67e885715f459c1294885410d65ead8de54cc9ef
Rishabh Shairy <rishusinha8@gmail.com>
2021-06-14 16:29:22 +0000

    [LeetCode Sync] Runtime - 19 ms (42.49%), Memory - 39 MB (100.00%)

---
+++ my-folder/0322-coin-change/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+                int n = coins.length;
+        int[][] tdp = new int[n + 1][amount + 1];
+
+        // intialize
+        for (int i = 0; i < amount + 1; i++) {
+            tdp[0][i] = Integer.MAX_VALUE - 1;
+        }
+        for (int i = 1; i < n + 1; i++) {
+            tdp[i][0] = 0;
+        }
+
+        //evaluate dp
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < amount + 1; j++) {
+                if (coins[i - 1] <= j) {
+                    tdp[i][j] = Math.min(1 + tdp[i][j - coins[i - 1]], tdp[i - 1][j]);
+                } else {
+                    tdp[i][j] = tdp[i - 1][j];
+                }
+
+            }
+        }
+        return tdp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : tdp[n][amount];
+    }
+}
