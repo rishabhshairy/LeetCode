511ddb01e0ba27d5bd8a6fe31c72fbb4bf5acb3b
Rishabh Shairy <rishusinha8@gmail.com>
2021-07-07 16:58:28 +0000

    [LeetCode Sync] Runtime - 4 ms (95.22%), Memory - 40.8 MB (100.00%)

---
+++ my-folder/0155-min-stack/solution.java
@@ -0,0 +1,43 @@
+class MinStack {
+
+    /** initialize your data structure here. */
+    Stack<Integer> st=new Stack<>();
+    int min=Integer.MAX_VALUE;
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if(val<=min){
+            st.push(min);
+            min=val;
+        }
+        st.push(val);
+    }
+    
+    public void pop() {
+        if(st.peek() == min){
+            st.pop();
+            min=st.pop();
+        } else{
+            st.pop();
+        }
+    }
+    
+    public int top() {
+     return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
