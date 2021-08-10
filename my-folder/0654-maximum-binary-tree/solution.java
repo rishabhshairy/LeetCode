f78a9d76601a7be1b01635d5d5331ad13e01019b
Rishabh Shairy <rishusinha8@gmail.com>
2021-08-10 17:47:00 +0000

    [LeetCode Sync] Runtime - 2 ms (58.68%), Memory - 39 MB (100.00%)

---
+++ my-folder/0654-maximum-binary-tree/solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return construct(nums, 0, nums.length - 1);
+
+    }
+
+    private TreeNode construct(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new TreeNode(nums[start]);
+        }
+
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (nums[maxIndex] < nums[i]) {
+                maxIndex = i;
+            }
+        }
+        TreeNode root = new TreeNode(nums[maxIndex]);
+        root.left = construct(nums, start, maxIndex - 1);
+        root.right = construct(nums, maxIndex + 1, end);
+        return root;
+    }
+}
