760cf7cd0913e1c4ec3bf87a69ac6cfd3bef6678
Rishabh Shairy <rishusinha8@gmail.com>
2021-08-31 19:32:41 +0000

    [LeetCode Sync] Runtime - 393 ms (95.87%), Memory - 0B (100.00%)

---
+++ my-folder/0197-rising-temperature/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT W1.Id
+FROM Weather AS W1 JOIN Weather AS W2 
+ON TIMESTAMPDIFF(DAY,W2.recordDate,W1.recordDate)=1
+WHERE W1.Temperature > W2.Temperature
