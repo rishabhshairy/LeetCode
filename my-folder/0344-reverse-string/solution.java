f78f7539abdbf17bcc236bd292d2adc482feee22
Rishabh Shairy <rishusinha8@gmail.com>
2021-09-02 16:14:49 +0000

    [LeetCode Sync] Runtime - 4 ms (5.03%), Memory - 47.6 MB (65.09%)

--- my-folder/0344-reverse-string/solution.java
+++ my-folder/0344-reverse-string/solution.java
@@ -1,19 +1,24 @@
 class Solution {
     public void reverseString(char[] s) {
-		int leftIndex = 0;
-		int rightIndex = s.length - 1;
-		char leftChar = 0;
-		char rightChar = 0;
-		for (int i = 0; i < s.length; i++) {
-			leftChar = s[leftIndex];
-			rightChar = s[rightIndex];
-			s[leftIndex] = rightChar;
-			s[rightIndex] = leftChar;
-			if (rightIndex - leftIndex <= 1) {
-				break;
-			}
-			leftIndex++;
-			rightIndex--;
-		}
+        if (s.length == 0) {
+            return;
+        }
+        int start = 0;
+        int end = s.length - 1;
+        solve(s, start, end);
+        System.out.println(s);
+    }
+
+    private void solve(char[] s, int start, int end) {
+        if (start == s.length / 2) {
+            return;
+        }
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+
+        start++;
+        end--;
+        solve(s,start,end);
     }
 }
