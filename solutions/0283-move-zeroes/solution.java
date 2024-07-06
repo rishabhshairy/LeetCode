f17ff75beb41b6f367c7f1ccb1f032039d1c3bc1
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-06 06:30:22 +0000

    [LeetCode Sync] Runtime - 2 ms (83.99%), Memory - 46.2 MB (6.45%)

--- solutions/0283-move-zeroes/solution.java
+++ solutions/0283-move-zeroes/solution.java
@@ -1,20 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        		int temp;
-		int ptr1 = 0;
-		for (int ptr2 = ptr1 + 1; ptr2 < nums.length; ptr2++) {
-			if (nums[ptr1] != 0 && nums[ptr2] != 0) {
-				ptr1++;
-			} else if (nums[ptr1] == 0 && nums[ptr2] == 0) {
-				continue;
-			} else if (nums[ptr1] == 0 && nums[ptr2] != 0) {
-				temp = nums[ptr1];
-				nums[ptr1] = nums[ptr2];
-				nums[ptr2] = temp;
-				ptr1++;
-			} else {
-				ptr1++;
-			}
-		}
+        int zeros = 0;
+        int p2 = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeros++;
+            } else {
+                nums[p2++] = nums[i];
+            }
+        }
+        // fill zeros at end
+        for (int i = 0; i < zeros; i++) {
+            nums[p2++] = 0;
+        }
     }
 }
