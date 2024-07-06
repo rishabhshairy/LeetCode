d214b3ca657ca0cb49bc5734f865c0c9c12cc562
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-06 15:23:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (67.35%)

--- solutions/0075-sort-colors/solution.java
+++ solutions/0075-sort-colors/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public void sortColors(int[] nums) {
-               int[] count = new int[3];
+        int[] count = new int[3];
         for (int i = 0; i < nums.length; i++) {
             count[nums[i]]++;
         }
