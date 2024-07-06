142b380fbecef80b694387b57b236abf3789dfca
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-06 07:23:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45 MB (84.59%)

--- solutions/0268-missing-number/solution.java
+++ solutions/0268-missing-number/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n=nums.length;
-        int nSum = (n*(n+1))/2;
-        
-        int arrSum=IntStream.of(nums).sum();
-        
-        return nSum-arrSum;
+        int sum = (nums.length * (nums.length + 1)) / 2;
+        int runSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            runSum += nums[i];
+        }
+        return sum - runSum;
     }
 }
