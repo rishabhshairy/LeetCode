f0c0f4638303847491dc212696297a84adf92188
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-13 13:27:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (83.56%)

--- solutions/0033-search-in-rotated-sorted-array/solution.java
+++ solutions/0033-search-in-rotated-sorted-array/solution.java
@@ -1,35 +1,38 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
-            return -1;
-        }
+        int n = nums.length;
+        return findIndex(nums, target, n);
+    }
+        public static int findIndex(int[] nums,int target, int n) {
+        int low = 0;
+        int high = n-1;
+        int ans = -1;
 
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        while (low <= high) {
+            int mid = (low+high)/2;
 
             if (nums[mid] == target) {
-                return mid;
+                ans  = mid;
             }
-
-            // check left bias
-
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
+            // left sorted
+            if (nums[low] <= nums[mid]) {
+                // check if element is between low and mid
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
                 } else {
-                    left = mid + 1;
+                    low = mid + 1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                
+            } // right sorted
+                else {
+                    // check if target is between mid and high
+                    if (nums[mid] <= target && target <= nums[high]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
                 }
-            }
         }
-        return -1;
+        return ans;
     }
 }
