1228b54b11c049149c6d0494f87ffe9c5fb31c68
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-15 18:38:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 50.5 MB (22.82%)

--- solutions/0540-single-element-in-a-sorted-array/solution.java
+++ solutions/0540-single-element-in-a-sorted-array/solution.java
@@ -1,31 +1,39 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-                int left = 0;
-        int right = nums.length - 1;
-
-        if (right == 0) {
+        int n = nums.length;
+        if (n == 1) {
             return nums[0];
         }
 
         if (nums[0] != nums[1]) {
             return nums[0];
+
         }
-        if (nums[nums.length - 1] != nums[nums.length - 2]) {
-            return nums[nums.length - 1];
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
         }
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if ((nums[mid + 1] != nums[mid]) && (nums[mid - 1] != nums[mid])) {
+        // reducing search space
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            // direct check on mid
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
 
-            if ((mid % 2 != 0 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                left = mid + 1;
+            // check whether we are at even or odd index
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) ||
+                    (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                low = mid + 1;
             } else {
-                right = mid - 1;
+                high = mid - 1;
             }
         }
         return -1;
+
     }
 }
