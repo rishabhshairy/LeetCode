e356ec1c2fb725aceb5a75d91198123bd6663175
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-18 04:19:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.6 MB (74.14%)

--- solutions/1646-kth-missing-positive-number/solution.java
+++ solutions/1646-kth-missing-positive-number/solution.java
@@ -1,13 +1,23 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for (int num :
-                arr) {
-            if (num <= k) {
-                k++;
+                int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // vvimp calculation
+            // generic --> arr[idx] - (idx+1)
+            int missing = arr[mid] - (mid + 1);
+
+            // eliminate left half if missing is less than K
+            // it means number of missing numbers should be ahead of current index
+            if (missing < k) {
+                low = mid + 1;
             } else {
-                break;
+                high = mid - 1;
             }
         }
-        return k;
+        // return low+k;
+        return (high + 1 + k);
     }
 }
