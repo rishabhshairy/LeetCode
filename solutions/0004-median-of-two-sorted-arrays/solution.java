b819f2a70a698214b7498b2ec1c7aa650970c1d7
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-19 14:06:59 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 46 MB (41.77%)

--- solutions/0004-median-of-two-sorted-arrays/solution.java
+++ solutions/0004-median-of-two-sorted-arrays/solution.java
@@ -1,34 +1,55 @@
 class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
-        double m1 = -1, m2 = -1;
-        int i = 0, j = 0, count = 0;
-        int n=a.length;
-        int m=b.length;
-        
+        int n1 = a.length;
+        int n2 = b.length;
 
-        if ((m + n) % 2 != 0) {
-            for (count = 0; count <= (m + n) / 2; count++) {
-                if (i != n && j != m) {
-                     m1 = (a[i] > b[j]) ? b[j++] : a[i++];
-                } else if (i < n) {
-                    m1 = a[i++];
-                } else {
-                    m1 = b[j++];
-                }
-            }
-            return m1;
-        } else {
-            for (count = 0; count <= (n + m) / 2; count++) {
-                m2 = m1;
-                if (i != n && j != m) {
-                   m1 = (a[i] > b[j]) ? b[j++] : a[i++];
-                } else if (i < n) {
-                    m1 = a[i++];
-                } else {
-                    m1 = b[j++];
-                }
-            }
-            return (m1 + m2) / 2;
+        // swap arrays if n1 > n2 to always perform ops on smaller array
+        if (n1 > n2) {
+            return findMedianSortedArrays(b, a);
         }
+
+        // vars for binary search
+        int totalLengthOfMergedArray = n1 + n2; // total length
+        int low = 0;
+        int high = n1;
+        int oddLeft = (n1 + n2 + 1) / 2; // finding no of elements to take from right
+        while (low <= high) {
+
+            int mid1 = (low + high) >> 1; // to be used on smaller array --> primarily left array
+            int mid2 = oddLeft - mid1; // to be used on larger array --> primarily right array
+            int left1 = Integer.MIN_VALUE, left2 = Integer.MIN_VALUE, right1 = Integer.MAX_VALUE,
+                    right2 = Integer.MAX_VALUE;
+
+            // check for boundary conditions
+            if (mid1 < n1) {
+                right1 = a[mid1]; // setting value on right half of smaller array
+            }
+            if (mid2 < n2) {
+                right2 = b[mid2]; // setting value on right half of larger array
+            }
+
+            if (mid1 - 1 >= 0) {
+                left1 = a[mid1 - 1]; // setting value of max left on left half of smaller array
+            }
+            if (mid2 - 1 >= 0) {
+                left2 = b[mid2 - 1]; // setting value of max left on left half of larger array
+            }
+
+            // now cross compare
+            if (left1 <= right2 && left2 <= right1) {
+                if (totalLengthOfMergedArray % 2 == 1) {
+                    return (double) Math.max(left1, left2);
+                } else {
+                    return (double) (Math.max(left1, left2) + Math.min(right1, right2)) / 2;
+                }
+            } else if (left1 > right2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+
+        }
+
+        return 0;
     }
 }
