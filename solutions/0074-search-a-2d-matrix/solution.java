25613af1f3438d85657c164a08e56318876bf8c1
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-20 17:07:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.2 MB (40.37%)

--- solutions/0074-search-a-2d-matrix/solution.java
+++ solutions/0074-search-a-2d-matrix/solution.java
@@ -1,22 +1,32 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-            if (matrix == null || matrix.length == 0) {
-                return false;
-            }
-            int start = 0, rows = matrix.length, cols = matrix[0].length;
-            int end = rows * cols - 1;
-            while (start <= end) {
-                int mid = (start + end) / 2;
-                if (matrix[mid / cols][mid % cols] == target) {
-                    return true;
-                } 
-                if (matrix[mid / cols][mid % cols] < target) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
-                }
-            }
+        if (matrix == null || matrix.length == 0) {
             return false;
+        }
 
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        int low = 0;
+        int high = (row * col) - 1; // assuming 1D matrix hypothetically
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int mappedRow = mid / col;
+            int mappedCol = mid % col;
+            int mappedNumber = matrix[mappedRow][mappedCol];
+
+            if (mappedNumber == target) {
+                return true;
+            } else if (mappedNumber > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        return false;
     }
 }
