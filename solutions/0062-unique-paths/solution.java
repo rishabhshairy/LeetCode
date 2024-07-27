0a732c1ea2d8fb61c671a83a8a965ed45be97d2d
Rishabh Shairy <rishusinha8@gmail.com>
2024-07-27 10:37:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.7 MB (19.58%)

---
+++ solutions/0062-unique-paths/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            int[] temp = new int[n];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    temp[j] = 1;
+                    continue;
+                }
+                int up = i > 0 ? dp[j] : 0;
+                int left = j > 0 ? temp[j - 1] : 0;
+                temp[j] = up + left;
+
+            }
+            dp = temp;
+        }
+        return dp[n - 1];
+    }
+}
