c1a73b44f15555f29a66781032f937a039787757
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-02 18:01:07 +0000

    [LeetCode Sync] Runtime - 1849 ms (8.82%), Memory - 41.4 MB (35.44%)

--- solutions/0698-partition-to-k-equal-sum-subsets/solution.java
+++ solutions/0698-partition-to-k-equal-sum-subsets/solution.java
@@ -1,39 +1,51 @@
 class Solution {
-public boolean canPartitionKSubsets(int[] nums, int k) {
-	if (k > nums.length) return false;
-	int sum = 0;
-	for (int i : nums) sum += i;
-	// basic logic test
-	if (sum % k != 0) return false;
-	Arrays.sort(nums);
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
 
-	return dfs(nums, new boolean[nums.length], k, 0, sum / k, nums.length - 1);
-}
+        if (totalSum % k == 0) {
+            int[] visited = new int[nums.length];
+            int halfSum = totalSum / k;
+            return findSubsetSum(nums, k, 0, 0, 0, halfSum, visited);
+        }
+        return false;
+    }
 
-private boolean dfs(int[] nums, boolean[] visited, int k, int currentSum, int targetSum, int position) {
-	// if k is 0, for sure nothing will be left unvisited! this is conclusion from simple math.
-	if (k == 0) return true;
+    private static boolean findSubsetSum(int[] nums, int k, int numCompleted, int currentSum, int index, int target,
+            int[] visited) {
 
-	// begin next sum search. Critical point: start search from nums.length - 1, not position!!!
-	if (currentSum == targetSum) return dfs(nums, visited, k - 1, 0, targetSum, nums.length - 1);
+        // Return true if we have successfully formed k-1 subsets
+        // as the last subset will for sure can be formed
+        if (numCompleted == k) {
+            return true;
+        }
 
-	for (int i = position; i >= 0; i--) {
-		// Skip logic 1: 
-		// Of course you cannot visit what's already visited.
-		if (visited[i]) continue;
-		// Skip logic 2:
-		// if the last position (i + 1) is not visited, that means it does not work for current combination, 
-		// and of course this position (i) has same value, it won't work as well, skip it.
-		if (i + 1 < nums.length && nums[i] == nums[i + 1] && !visited[i + 1]) continue;
-		// Skip logic 3:
-		// No need to explain, just out of range case.
-		if (currentSum + nums[i] > targetSum) continue;
+        if (currentSum > target) {
+            return false;
+        }
 
-		// simple recursion and backtracking
-		visited[i] = true;
-		if (dfs(nums, visited, k, currentSum + nums[i], targetSum, i - 1)) return true;
-		visited[i] = false;
-	}
-	return false;
-}
+        // When successfully formed one subset find another subset from index 0
+        if (currentSum == target) {
+            return findSubsetSum(nums, k, numCompleted + 1, 0, 0, target, visited);
+        }
+
+        // Try out each possibility to group into each subset
+        for (int i = index; i < nums.length; i++) {
+            // try nums[i] if not visited
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                //currentSum += nums[i];
+                // We only need to try out i+1, i+2 elements and onwards
+                // as the previous elements have already been tried out in previous backtrack
+                // loops
+                if (findSubsetSum(nums, k, numCompleted, currentSum + nums[i], i + 1, target, visited)) {
+                    return true;
+                }
+                visited[i] = 0;
+            }
+        }
+        return false;
+    }
 }
