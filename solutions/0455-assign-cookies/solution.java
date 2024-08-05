d3420d06bd081dde067566622ebff6e63068f34a
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-05 14:23:02 +0000

    [LeetCode Sync] Runtime - 9 ms (99.67%), Memory - 45.1 MB (100.00%)

---
+++ solutions/0455-assign-cookies/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+                Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+        int result = 0;
+
+        while (child < g.length && cookie < s.length) {
+            if (g[child] <= s[cookie]) {
+                child++;
+                cookie++;
+                result++;
+            } else {
+                cookie++;
+            }
+        }
+
+        return result;
+    }
+}
