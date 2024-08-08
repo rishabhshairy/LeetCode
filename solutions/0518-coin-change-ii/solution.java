5ead3ab8b10603168c0814ed6a37bc9b7a024d78
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-08 18:26:38 +0000

    [LeetCode Sync] Runtime - 5 ms (78.15%), Memory - 48.5 MB (55.41%)

--- solutions/0518-coin-change-ii/solution.java
+++ solutions/0518-coin-change-ii/solution.java
@@ -1,24 +1,37 @@
 class Solution {
-    public int change(int n, int[] S) {
-        int m = S.length;
-        int[][] tdp = new int[m + 1][n + 1];
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
 
-        for (int i = 0; i < n + 1; i++) {
-            tdp[0][i] = 0;
-        }
-        for (int i = 0; i < m + 1; i++) {
-            tdp[i][0] = 1;
+        for (int[] row :
+                dp) {
+            Arrays.fill(row, -1);
         }
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (S[i - 1] <= j) {
-                    tdp[i][j] = tdp[i - 1][j] + tdp[i][j - S[i - 1]];
-                } else {
-                    tdp[i][j] = tdp[i - 1][j];
-                }
+        return countWaysRecMemo(n - 1, coins, amount, dp);
+    }
+
+        private static int countWaysRecMemo(int index, int[] denominations, int value, int[][] dp) {
+
+        // base case
+        if (index == 0) {
+            if (value % denominations[index] == 0) {
+                return 1;
             }
+            return 0;
         }
-        return tdp[m][n];
+
+        if (dp[index][value] != -1) {
+            return dp[index][value];
+        }
+
+        int notPick = countWaysRecMemo(index - 1, denominations, value, dp);
+        int pick = 0;
+        if (denominations[index]<=value){
+            pick = countWaysRecMemo(index, denominations, value - denominations[index], dp);
+
+        }
+
+        return dp[index][value] = notPick + pick;
     }
 }
