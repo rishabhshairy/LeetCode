9b43948c819dcf6a668c8276e2656c87f3844a0c
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-08 16:06:49 +0000

    [LeetCode Sync] Runtime - 110 ms (7.02%), Memory - 45 MB (26.72%)

--- solutions/0322-coin-change/solution.java
+++ solutions/0322-coin-change/solution.java
@@ -1,27 +1,31 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] tdp = new int[n + 1][amount + 1];
+    public int coinChange(int[] num, int target) {
+        // Step 1 --> declare DP array
+        int n = num.length;
+        int[][] dp = new int[n][target + 1];
 
-        // intialize
-        for (int i = 0; i < amount + 1; i++) {
-            tdp[0][i] = Integer.MAX_VALUE - 1;
-        }
-        for (int i = 1; i < n + 1; i++) {
-            tdp[i][0] = 0;
-        }
-
-        //evaluate dp
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < amount + 1; j++) {
-                if (coins[i - 1] <= j) {
-                    tdp[i][j] = Math.min(1 + tdp[i][j - coins[i - 1]], tdp[i - 1][j]);
-                } else {
-                    tdp[i][j] = tdp[i - 1][j];
-                }
-
+        // Step 2 --> base condition
+        // why looping from 0 --> target == dp[0][target] needs to be checked for each
+        // a[0]
+        for (int i = 0; i <= target; i++) {
+            if (i % num[0] == 0) {
+                dp[0][i] = i / num[0];
+            } else {
+                dp[0][i] = (int) Math.pow(10, 9);
             }
         }
-        return tdp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : tdp[n][amount];
+        // Step 3 --> form loops
+        for (int index = 1; index < n; index++) {
+            for (int currTarget = 0; currTarget <= target; currTarget++) {
+                int notPick = dp[index - 1][currTarget];
+                int pick = (int) Math.pow(10, 9);
+                if (num[index] <= currTarget) {
+                    pick = 1 + dp[index][currTarget - num[index]];
+                }
+                dp[index][currTarget] = Math.min(notPick, pick);
+            }
+        }
+
+        return dp[n - 1][target] != (int) Math.pow(10,9) ? dp[n - 1][target] : -1;
     }
 }
