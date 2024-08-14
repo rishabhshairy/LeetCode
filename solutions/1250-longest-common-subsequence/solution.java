e0b87bdc163fa0b62f94f5bd09ce028ca25ec149
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-14 16:01:58 +0000

    [LeetCode Sync] Runtime - 25 ms (29.23%), Memory - 50.9 MB (45.74%)

--- solutions/1250-longest-common-subsequence/solution.java
+++ solutions/1250-longest-common-subsequence/solution.java
@@ -1,26 +1,32 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-        int n = s1.length();
-        int m = s2.length();
+    public int longestCommonSubsequence(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
 
-        int[][] tdp = new int[n + 1][m + 1];
-
-        for (int[] row :
-                tdp) {
-            Arrays.fill(row, 0);
+        int[][] dp = new int[n1][n2];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
 
-        // evluate LCS
+        return solve(n1 - 1, n2 - 1, s, t, dp);
+    }
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    tdp[i][j] = 1 + tdp[i - 1][j - 1];
-                } else {
-                    tdp[i][j] = Math.max(tdp[i - 1][j], tdp[i][j - 1]);
-                }
-            }
+    private static int solve(int index1, int index2, String s, String t, int[][] dp) {
+        if (index1 < 0 || index2 < 0) {
+            return 0;
         }
-        return tdp[n][m] == -1 ? 0 : tdp[n][m];
+
+        if (dp[index1][index2] != -1) {
+            return dp[index1][index2];
+        }
+
+        // match case
+        if (s.charAt(index1) == t.charAt(index2)) {
+            return dp[index1][index2] = 1 + solve(index1 - 1, index2 - 1, s, t, dp);
+        }
+        // non Match case
+        // move either index for 2 way possibilities
+        return dp[index1][index2] = Math.max(solve(index1 - 1, index2, s, t, dp),
+                solve(index1, index2 - 1, s, t, dp));
     }
 }
