ef85253cde78055ec969d7dbf749e666b65e8a9a
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-15 07:54:58 +0000

    [LeetCode Sync] Runtime - 59 ms (33.83%), Memory - 55.3 MB (33.53%)

--- solutions/0516-longest-palindromic-subsequence/solution.java
+++ solutions/0516-longest-palindromic-subsequence/solution.java
@@ -3,20 +3,17 @@ class Solution {
         String rev = new StringBuilder(s).reverse().toString();
         int n = s.length();
 
-        int[][] tdp = new int[n + 1][n + 1];
+        int[][] dp = new int[n + 1][n + 1];
 
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == 0 || j == 0) {
-                    tdp[i][j] = 0;
-                } else if (s.charAt(i - 1) == rev.charAt(j - 1)) {
-                    tdp[i][j] = 1 + tdp[i - 1][j - 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    tdp[i][j] = Math.max(tdp[i - 1][j], tdp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        return tdp[n][n];
+        return dp[n][n];
     }
 }
