90cd440ffd211e866ad2d671d97dc2b51cadcd26
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-17 08:41:01 +0000

    [LeetCode Sync] Runtime - 5 ms (66.59%), Memory - 44.8 MB (79.78%)

---
+++ solutions/0072-edit-distance/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minDistance(String str1, String str2) {
+        int n = str1.length();
+        int m = str2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
+        // base case
+
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < m + 1; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    // 3 ops
+                    int insert = 1 + dp[i][j - 1];
+                    int delete = 1 + dp[i - 1][j];
+                    int replace = 1 + dp[i - 1][j - 1];
+
+                    dp[i][j] = Math.min(insert, Math.min(delete, replace));
+
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+}
