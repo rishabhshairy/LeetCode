f80fc8ac9583e8753185c41e5650f9fc7ddeac79
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-17 15:22:01 +0000

    [LeetCode Sync] Runtime - 246 ms (26.08%), Memory - 56.4 MB (5.01%)

--- solutions/0005-longest-palindromic-substring/solution.java
+++ solutions/0005-longest-palindromic-substring/solution.java
@@ -1,41 +1,38 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() <= 1) {
-            return s;
-        }
+        String rev = new StringBuilder(s).reverse().toString();
+
         int n = s.length();
-        boolean[][] tdp = new boolean[n][n];
+        int m = rev.length();
+        int[][] dp = new int[n + 1][m + 1];
+        int maxPalindromeSubstringLength = 0;
+        int endIndex = 0;
 
-        int longest = 1;
-        int start = 0;
+        int rowP = 0, colP = 0;
 
-        for (int i = 0; i < n; i++) {
-            tdp[i][i] = true;
-        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    if (maxPalindromeSubstringLength < dp[i][j]) {
 
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                tdp[i][i + 1] = true;
-                start = i;
-                longest = 2;
-            }
-        }
+                        int iStart = i - dp[i][j];
+                        int jEnd = j;
 
-        for (int len = 3; len <= n; len++) {
-            for (int i = 0; i < n - len + 1; i++) {
-                int j = i + len - 1;
+                        if (iStart == s.length() - jEnd) {
+                            maxPalindromeSubstringLength = dp[i][j];
+                            endIndex = i;
+                        }
 
-                if (s.charAt(i) == s.charAt(j) && tdp[i + 1][j - 1]) {
-                    tdp[i][j] = true;
-                    if (len > longest) {
-                        start = i;
-                        longest = len;
                     }
+                } else {
+                    dp[i][j] = 0;
                 }
             }
-
         }
 
-        return s.substring(start, start + longest);
+        // System.out.println(maxPalindromeSubstringLength);
+
+        return s.substring(endIndex - maxPalindromeSubstringLength, endIndex);
     }
 }
