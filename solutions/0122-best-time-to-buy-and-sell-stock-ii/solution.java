d32a6da594edbdfe8ebd91571f6e489621e9d261
Rishabh Shairy <rishusinha8@gmail.com>
2024-08-22 14:32:12 +0000

    [LeetCode Sync] Runtime - 1 ms (92.03%), Memory - 46 MB (18.47%)

--- solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
+++ solutions/0122-best-time-to-buy-and-sell-stock-ii/solution.java
@@ -1,12 +1,29 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-		int maxCost = 0;
+    public int maxProfit(int[] values) {
+        int n = values.length;
+        long[][] dp = new long[n + 1][2];
+        long profit = 0;
 
-		for (int i = 1; i < prices.length; i++) {
-            if(prices[i]>prices[i-1]){
-                maxCost += prices[i] - prices[i-1];
+        // base case --> When we have no stock to buy/sell
+        dp[n][0] = dp[n][1] = 0;
+
+        for (int index = n - 1; index >= 0; index--) {
+            for (int canBuy = 0; canBuy <= 1; canBuy++) {
+
+                if (canBuy == 0) {
+                    long buy = -values[index] + dp[index + 1][1];
+                    long notBuy = dp[index + 1][0];
+                    profit = Math.max(buy, notBuy);
+                }
+
+                if (canBuy == 1) { // selling stocks
+                    long sell = values[index] + dp[index + 1][0];
+                    long notSell = dp[index + 1][1];
+                    profit = Math.max(sell, notSell);
+                }
+                dp[index][canBuy] = profit;
             }
-		}
-        return maxCost;
+        }
+        return (int)dp[0][0];
     }
 }
