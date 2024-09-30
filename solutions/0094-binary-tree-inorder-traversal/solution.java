6d4ead132600b6d85de468ec86c58640a2814d0d
Rishabh Shairy <rishusinha8@gmail.com>
2024-09-30 18:13:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (17.71%)

--- solutions/0094-binary-tree-inorder-traversal/solution.java
+++ solutions/0094-binary-tree-inorder-traversal/solution.java
@@ -1,34 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        if (root == null) {
-            return answer;
+        List<Integer> inorderList = new ArrayList<>();
+        Stack<TreeNode> nodeStack = new Stack<>();
+        TreeNode node = root;
+        while (true) {
+            if (node != null) {
+                nodeStack.push(node);
+                node = node.left;
+            } else {
+                if (nodeStack.isEmpty()) {
+                    break;
+                }
+                node = nodeStack.pop();
+                inorderList.add(node.val);
+                node = node.right;
+            }
         }
-        traverse(root, answer);
-        return answer;
-    }
-
-    private static void traverse(TreeNode root, List<Integer> answer) {
-        if (root == null) {
-            return;
-        }
-        traverse(root.left, answer);
-        answer.add(root.val);
-        traverse(root.right, answer);
+        return inorderList;
     }
 }
