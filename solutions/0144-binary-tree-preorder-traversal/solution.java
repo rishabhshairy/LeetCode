7a73874d5a806e2fb63889c56fe4b5bca7e7b475
Rishabh Shairy <rishusinha8@gmail.com>
2024-09-30 15:15:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.3 MB (82.94%)

--- solutions/0144-binary-tree-preorder-traversal/solution.java
+++ solutions/0144-binary-tree-preorder-traversal/solution.java
@@ -1,34 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
-        if (root == null) {
-            return answer;
-        }
-        traverse(root, answer);
-        return answer;
+        List<Integer> preOrderList = new ArrayList<>();
+        traversePreOrder(root, preOrderList);
+        return preOrderList;
     }
 
-    private static void traverse(TreeNode root, List<Integer> answer) {
+    private static void traversePreOrder(TreeNode root, List<Integer> preOrderList) {
+
         if (root == null) {
             return;
         }
-        answer.add(root.val);
-        traverse(root.left, answer);
-        traverse(root.right, answer);
+
+        preOrderList.add(root.val);
+        traversePreOrder(root.left, preOrderList);
+        traversePreOrder(root.right, preOrderList);
     }
 }
