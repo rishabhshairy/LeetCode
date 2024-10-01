d2069aec651357b39348acff8ece2d16c2735f42
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-01 15:09:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (49.78%)

--- solutions/0145-binary-tree-postorder-traversal/solution.java
+++ solutions/0145-binary-tree-postorder-traversal/solution.java
@@ -1,34 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> postOrderList = new ArrayList<>();
         if (root == null) {
-            return answer;
+            return postOrderList;
         }
-        traverse(root, answer);
-        return answer;
-    }
 
-    private static void traverse(TreeNode root, List<Integer> answer) {
-        if (root == null) {
-            return;
+        Stack<TreeNode> opStack = new Stack<>();
+        Stack<TreeNode> resultStack = new Stack<>();
+        opStack.push(root);
+
+        while (!opStack.isEmpty()) {
+            root = opStack.pop();
+            if (root.left != null) {
+                opStack.push(root.left);
+            }
+
+            if (root.right != null) {
+                opStack.push(root.right);
+            }
+            postOrderList.add(root.val);
         }
-        traverse(root.left, answer);
-        traverse(root.right, answer);
-        answer.add(root.val);
+        Collections.reverse(postOrderList);
+        return postOrderList;
     }
 }
