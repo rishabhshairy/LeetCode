0919c2de9a0345441cf7c15d07cbb050815e8a25
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-02 05:20:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.9 MB (96.91%)

--- solutions/0110-balanced-binary-tree/solution.java
+++ solutions/0110-balanced-binary-tree/solution.java
@@ -1,32 +1,39 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root==null){
-            return true;
-        }
-        int leftH=height(root.left);
-        int rightH=height(root.right);
-        
-        return (Math.abs(leftH-rightH)<=1 && isBalanced(root.left)&&isBalanced(root.right));
+        return dfsHeight(root) != -1;
     }
-    private int height(TreeNode root) {
+
+    private int dfsHeight(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        return 1 + Math.max(height(root.left), height(root.right));
+
+        int leftHeight = dfsHeight(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = dfsHeight(root.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
