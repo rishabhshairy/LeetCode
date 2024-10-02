9794ec579179a91ad5fa79d0e19d1ffb373ca0b2
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-02 18:17:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42 MB (63.93%)

--- solutions/0199-binary-tree-right-side-view/solution.java
+++ solutions/0199-binary-tree-right-side-view/solution.java
@@ -1,36 +1,35 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        if (root == null) {
-            return new ArrayList<>();
-        }
-        List<Integer> answer = new ArrayList<>();
-        solve(root, answer, 0);
-        return answer;
+        ArrayList<Integer> result = new ArrayList<>();
+        findRightView(root, 0, result);
+        return result;
     }
 
-    private void solve(TreeNode root, List<Integer> answer, int depth) {
-        if (root == null) {
+    private static void findRightView(TreeNode node, int level, ArrayList<Integer> result) {
+        if (node == null) {
             return;
         }
-        if (answer.size()==depth) {
-            answer.add(root.val);
+
+        if (level == result.size()) {
+            result.add(node.val);
         }
-        solve(root.right, answer, depth + 1);
-        solve(root.left, answer, depth + 1);
+
+        findRightView(node.right, level + 1, result);
+        findRightView(node.left, level + 1, result);
     }
 }
