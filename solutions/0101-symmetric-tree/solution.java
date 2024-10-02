241f80cf221bfb86746941a8a7d36dbc32c69a45
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-02 19:00:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (99.17%)

--- solutions/0101-symmetric-tree/solution.java
+++ solutions/0101-symmetric-tree/solution.java
@@ -1,37 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return traverse(root, root);
+        return root == null || isSymmetricHelp(root.left, root.right);
     }
 
-    private boolean traverse(TreeNode leftNode, TreeNode rightNode) {
-        if (leftNode == null && rightNode == null) {
-            return true;
+    private boolean isSymmetricHelp(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
         }
-        if (leftNode == null || rightNode == null) {
+
+        if (left.val != right.val) {
             return false;
         }
 
-        if (!traverse(leftNode.left, rightNode.right)) {
-            return false;
-        }
-        if (leftNode.val != rightNode.val) {
-            return false;
-        }
-        return traverse(leftNode.right, rightNode.left);
+        return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
     }
+
 }
