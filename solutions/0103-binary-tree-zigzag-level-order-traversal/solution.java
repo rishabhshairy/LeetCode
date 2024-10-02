b41fd03f928f2d21815aa68e6f97dea96c3e2695
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-02 08:04:25 +0000

    [LeetCode Sync] Runtime - 1 ms (71.46%), Memory - 42.2 MB (27.28%)

--- solutions/0103-binary-tree-zigzag-level-order-traversal/solution.java
+++ solutions/0103-binary-tree-zigzag-level-order-traversal/solution.java
@@ -1,51 +1,49 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> answer = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         if (root == null) {
-            return answer;
+            return new ArrayList<>();
         }
-        int count = 1;
-        solve(root, answer, count);
-        return answer;
+        boolean directionFlag = true;
+        Queue<TreeNode> nodeQueue = new ArrayDeque<>();
+        nodeQueue.offer(root);
+
+        while (!nodeQueue.isEmpty()) {
+            int size = nodeQueue.size();
+            Integer[] levelList = new Integer[size];
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = nodeQueue.poll();
+                // below logic handles the direction of insertion into the list
+                int index = directionFlag ? i : (size - 1 - i);
+                levelList[index] = node.val;
+
+                if (node.left != null) {
+                    nodeQueue.offer(node.left);
+                }
+                if (node.right != null) {
+                    nodeQueue.offer(node.right);
+                }
+            }
+
+            directionFlag = !directionFlag;
+            result.add(Arrays.asList(levelList));
+        }
+        return result;
     }
-
-    private void solve(TreeNode root, List<List<Integer>> answer, int count) {
-        Queue<TreeNode> node = new LinkedList<>();
-        node.offer(root);
-        while (!node.isEmpty()) {
-            int queueSize = node.size();
-            List<Integer> nodeValList = new ArrayList<>();
-            System.out.println(count);
-            for (int i = 0; i < queueSize; i++) {
-                TreeNode tempNode = node.poll();
-                nodeValList.add(tempNode.val);
-
-                if (tempNode.left != null) {
-                    node.add(tempNode.left);
-                }
-                if (tempNode.right != null) {
-                    node.add(tempNode.right);
-                }
-            }
-            if (count % 2 == 0) {
-                Collections.reverse(nodeValList);
-            }
-            count++;
-            answer.add(nodeValList);
-    }}
 }
