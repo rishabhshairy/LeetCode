9bddbf7e21f7deb5f4a29c3b18606e5c81d90b99
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-07 16:17:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.3 MB (58.86%)

--- solutions/0098-validate-binary-search-tree/solution.java
+++ solutions/0098-validate-binary-search-tree/solution.java
@@ -1,37 +1,31 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-
     public boolean isValidBST(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-        if (root.left == null && root.right == null) {
-            return true;
-        }
-        return solve(root, null, null);
+        return check(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    private boolean solve(TreeNode root, Integer minValue, Integer maxVal) {
-        if (root == null) {
+    private boolean check(TreeNode node, long minValue, long maxValue) {
+        if (node == null) {
             return true;
         }
-        if ((minValue != null && root.val <= minValue) || (maxVal != null && root.val >= maxVal)) {
+
+        if (node.val <= minValue || node.val >= maxValue) {
             return false;
         }
-        return solve(root.left, minValue, root.val) && solve(root.right, root.val, maxVal);
+        return check(node.left, minValue, node.val) && check(node.right, node.val, maxValue);
     }
 }
