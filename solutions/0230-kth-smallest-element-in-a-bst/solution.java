33198a365ec9afb5454bbea97860ba5c5addfa70
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-07 16:02:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (25.97%)

--- solutions/0230-kth-smallest-element-in-a-bst/solution.java
+++ solutions/0230-kth-smallest-element-in-a-bst/solution.java
@@ -1,36 +1,37 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        int[] res = {0, -1};
-        solve(root, k, res);
-        return res[1];
+        int[] count = new int[1];
+        int[] smallest = new int[1];
+        findSmallest(root, count, smallest, k);
+        return smallest[0];
     }
 
-    private void solve(TreeNode root, int k, int[] res) {
-        if (root == null) {
+    private void findSmallest(TreeNode node, int[] count, int[] smallest, int k) {
+        if (node == null) {
             return;
         }
-        solve(root.left, k, res);
-        res[0] += 1;
-        if (res[0] == k) {
-            res[1] = root.val;
+
+        findSmallest(node.left, count, smallest, k);
+        count[0]++;
+        if (count[0] == k) {
+            smallest[0] = node.val;
             return;
         }
-        solve(root.right, k, res);
-        return;
+        findSmallest(node.right, count, smallest, k);
     }
 }
