2dbabac0cf97a88ad755fb9d5f8a00886899716f
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-11 06:02:03 +0000

    [LeetCode Sync] Runtime - 1 ms (45.71%), Memory - 51 MB (63.43%)

--- solutions/0975-range-sum-of-bst/solution.java
+++ solutions/0975-range-sum-of-bst/solution.java
@@ -1,45 +1,33 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        ArrayList<Integer> nums = new ArrayList<>();
-        int sum = 0;
-        solve(root, low, high, nums);
-        for (Integer num :
-                nums) {
-            sum += num;
-        }
-        return sum;
+        int[] sum = { 0 };
+        solveOptimal(root, low, high, sum);
+        return sum[0];
     }
 
-    private void solve(TreeNode root, int low, int high, ArrayList<Integer> nums) {
-        if (root.left == null && root.right == null) {
-            if (root.val >= low && root.val <= high) {
-                nums.add(root.val);
-            }
+    private void solveOptimal(TreeNode root, int low, int high, int[] sum) {
+        if (root == null) {
             return;
         }
+        solveOptimal(root.left, low, high, sum);
         if (root.val >= low && root.val <= high) {
-                nums.add(root.val);
-            }
-        if (root.left != null) {
-            solve(root.left, low, high, nums);
+            sum[0] += root.val;
         }
-        if (root.right!=null){
-            solve(root.right, low, high, nums);
-        }
+        solveOptimal(root.right, low, high, sum);
     }
 }
