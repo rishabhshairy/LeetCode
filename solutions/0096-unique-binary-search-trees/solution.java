a3101c9fc140fa8f77a1736df9d87ecc81679d5d
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-13 15:36:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40 MB (90.41%)

--- solutions/0096-unique-binary-search-trees/solution.java
+++ solutions/0096-unique-binary-search-trees/solution.java
@@ -1,15 +1,19 @@
 class Solution {
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
+        int[] dp = new int[n + 2];
 
-        for (int i = 1; i < n + 1; i++) {
-            if (i <= 2) {
-                dp[i] = i;
-                continue;
-            }
+        if (n <= 1) {
+            return 1;
+        }
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        long res = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = 0;
             for (int j = 0; j < i; j++) {
-                dp[i] += (dp[j] * dp[i - 1 - j]);
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
         return dp[n];
