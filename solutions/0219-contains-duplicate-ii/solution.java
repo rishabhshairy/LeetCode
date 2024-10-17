2085c0df665ead5314069390288da4fbc07b06e7
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-17 14:40:27 +0000

    [LeetCode Sync] Runtime - 21 ms (28.11%), Memory - 55.2 MB (88.77%)

--- solutions/0219-contains-duplicate-ii/solution.java
+++ solutions/0219-contains-duplicate-ii/solution.java
@@ -1,13 +1,25 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (hmap.containsKey(nums[i]) && (i - hmap.get(nums[i])) <= k) {
+        if (k == 0) {
+            return false;
+        }
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int index = 0; index < nums.length; index++) {
+
+            // checking if num already in set
+            if (numSet.contains(nums[index])) {
                 return true;
             }
-            hmap.put(nums[i], i);
 
+            // Adjust window size and remove first element from left
+            if (index >= k) {
+                int window = index - k;
+                numSet.remove(nums[window]);
+            }
+            numSet.add(nums[index]);
         }
+        
         return false;
     }
 }
