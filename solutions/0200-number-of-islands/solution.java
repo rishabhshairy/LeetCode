352fd9f5b4a0e40949269ec583f7493da5deaea8
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-21 13:20:27 +0000

    [LeetCode Sync] Runtime - 7 ms (19.31%), Memory - 52 MB (19.25%)

--- solutions/0200-number-of-islands/solution.java
+++ solutions/0200-number-of-islands/solution.java
@@ -1,34 +1,64 @@
 class Solution {
-    int row;
-    int col;
+    static class Pair {
+        int first;
+        int second;
+
+        public Pair(int _first, int _second) {
+            this.first = _first;
+            this.second = _second;
+        }
+    }
 
     public int numIslands(char[][] grid) {
-        row = grid.length;
-        col = grid[0].length;
-
+        int rowSize = grid.length;
+        int colSize = grid[0].length;
         int count = 0;
+        boolean[][] visited = new boolean[rowSize][colSize];
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
+        for (int row = 0; row < rowSize; row++) {
+            for (int col = 0; col < colSize; col++) {
+                if (grid[row][col] == '1' && !visited[row][col]) {
                     count++;
+                    bfs(row, col, visited, grid);
                 }
             }
         }
         return count;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-       if (i < 0 || j < 0 || i >= row || j>=col||grid[i][j] != '1') {
-            return;
-        }
-        grid[i][j] = '0';
+    private void bfs(int row, int col, boolean[][] visited, char[][] grid) {
+        visited[row][col] = true;
+        Queue<Pair> vertexQueue = new LinkedList<>();
+        vertexQueue.offer(new Pair(row, col));
+        int rowSize = grid.length;
+        int colSize = grid[0].length;
 
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1);
-        return;
+        while (!vertexQueue.isEmpty()) {
+            int currRow = vertexQueue.peek().first;
+            int currCol = vertexQueue.peek().second;
+            vertexQueue.poll();
+
+            // traverse neighbours and mark them visited
+
+            for (int deltaRow = -1; deltaRow <= 1; deltaRow++) {
+                for (int deltaCol = -1; deltaCol <= 1; deltaCol++) {
+                    int nrow = deltaRow + currRow;
+                    int ncol = deltaCol + currCol;
+
+                    if ((deltaRow == -1 && deltaCol == -1) || (deltaRow == -1 && deltaCol == 1)
+                            || (deltaRow == 1 && deltaCol == -1) || (deltaRow == 1 && deltaCol == 1)) {
+                        continue;
+                    }
+
+                    // boundary condition check
+                    if (nrow >= 0 && nrow < rowSize && ncol >= 0 && ncol < colSize
+                            && grid[nrow][ncol] == '1' && !visited[nrow][ncol]) {
+                        visited[nrow][ncol] = true;
+                        vertexQueue.add(new Pair(nrow, ncol));
+                    }
+                }
+            }
+
+        }
     }
 }
