831259e9bb12a42a6c8f40a4ce3ee10d5732a4b1
Rishabh Shairy <rishusinha8@gmail.com>
2024-10-28 16:47:43 +0000

    [LeetCode Sync] Runtime - 4 ms (90.51%), Memory - 44.9 MB (83.38%)

--- solutions/0207-course-schedule/solution.java
+++ solutions/0207-course-schedule/solution.java
@@ -1,41 +1,54 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
-        int[] visited = new int[numCourses];
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        // considering courses as vertex of graph
+        // and pre-req as directed edges
+        // Approach --> Check cycle in the formed graph
+
+        List<List<Integer>> adjacencyList = prepareDirectedGraph(prerequisites, numCourses);
+        boolean[] visited = new boolean[numCourses];
+        boolean[] pathVisited = new boolean[numCourses];
 
         for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
-        }
-
-        for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
-                if (checkCycle(visited, graph, i)) {
+            if (!visited[i]) {
+                if (checkCycleDFS(i, adjacencyList, visited, pathVisited)) {
                     return false;
                 }
             }
         }
-
         return true;
     }
 
-    private boolean checkCycle(int[] visited, ArrayList<ArrayList<Integer>> graph, int course) {
-        visited[course] = -1;
-        for (Integer i :
-                graph.get(course)) {
-            if (visited[i] == -1) {
-                return true;
-            } else if (visited[i] == 0) {
-                if (checkCycle(visited, graph, i)) {
+    private boolean checkCycleDFS(int currNode, List<List<Integer>> adjacencyList, boolean[] visited,
+            boolean[] pathVisited) {
+        visited[currNode] = true;
+        pathVisited[currNode] = true;
+
+        for (Integer adjNode : adjacencyList.get(currNode)) {
+            if (!visited[adjNode]) {
+                if (checkCycleDFS(adjNode, adjacencyList, visited, pathVisited)) {
                     return true;
                 }
+
+            } else if (pathVisited[adjNode]) {
+                return true;
             }
         }
-        visited[course] = 1;
+        pathVisited[currNode] = false;
         return false;
+    }
+
+    private List<List<Integer>> prepareDirectedGraph(int[][] edges, int vertex) {
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+
+        for (int i = 0; i < vertex; i++) {
+            adjacencyList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            adjacencyList.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        return adjacencyList;
     }
 }
