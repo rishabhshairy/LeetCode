f956bc329205374a7bfe783b58385c662854884c
Rishabh Shairy <rishusinha8@gmail.com>
2024-11-04 14:59:01 +0000

    [LeetCode Sync] Runtime - 5 ms (74.10%), Memory - 57.1 MB (90.37%)

--- solutions/0011-container-with-most-water/solution.java
+++ solutions/0011-container-with-most-water/solution.java
@@ -2,21 +2,19 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int area = Integer.MIN_VALUE;
-
-        if (height.length <= 1) {
-            return 0;
-        }
+        int res = 0;
+        int area = 0;
 
         while (left < right) {
-            area = Math.max(area, Math.min(height[left], height[right]) * (right - left));
-
-            if (height[left] < height[right]) {
+            area = Math.min(height[left], height[right]) * (right - left);
+            res = Math.max(res, area);
+            // since we want to maximize container--> difference of walls should be minimum
+            if (height[left] <= height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
-        return area;
+        return res;
     }
 }
