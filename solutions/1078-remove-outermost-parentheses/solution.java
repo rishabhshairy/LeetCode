45ec79a6a5ce15368e60fb6ca1d7634e2431e498
Rishabh Shairy <rishusinha8@gmail.com>
2024-11-09 10:19:03 +0000

    [LeetCode Sync] Runtime - 4 ms (62.24%), Memory - 43.4 MB (26.35%)

--- solutions/1078-remove-outermost-parentheses/solution.java
+++ solutions/1078-remove-outermost-parentheses/solution.java
@@ -1,22 +1,24 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-                int count = 0;
+        int count=0;
         StringBuilder sb = new StringBuilder();
 
-        for (Character ch :
-                s.toCharArray()) {
-            if (ch == '(') {
-                if (count != 0) {
+        for(Character ch: s.toCharArray()) {
+            if (ch =='('){
+                // append open bracket only when its enountered 2nd time 
+                if(count!=0){
                     sb.append(ch);
                 }
                 count++;
             } else {
+                // close the primitive
                 count--;
-                if (count != 0) {
+                if (count!=0) {
                     sb.append(ch);
                 }
             }
         }
+
         return sb.toString();
     }
 }
