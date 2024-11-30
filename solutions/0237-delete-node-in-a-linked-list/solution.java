96ac909ea20739f4e3a5f9570640d3b6f0458dae
Rishabh Shairy <rishusinha8@gmail.com>
2024-11-30 17:53:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.9 MB (52.38%)

--- solutions/0237-delete-node-in-a-linked-list/solution.java
+++ solutions/0237-delete-node-in-a-linked-list/solution.java
@@ -1,16 +1,15 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
  * }
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        node.val=0;
+        node.val = 0;
         node.val += node.next.val;
-        node.next=node.next.next;
-     //   System.out.print(node.next.val);
+        node.next = node.next.next;
     }
 }
