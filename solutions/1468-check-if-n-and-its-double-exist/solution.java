e9ad0f5ef8821f94b271869007d74d54fb23d664
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-01 08:37:24 +0000

    [LeetCode Sync] Runtime - 3 ms (27.19%), Memory - 43.3 MB (33.30%)

--- solutions/1468-check-if-n-and-its-double-exist/solution.java
+++ solutions/1468-check-if-n-and-its-double-exist/solution.java
@@ -1,16 +1,36 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            hs.put(arr[i],i);
-            
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            int target = 2 * arr[i];
+            int index = doBinarySearch(arr, target);
+
+            if (index >= 0 && index != i) {
+                return true;
+            }
         }
-        for(int i=0;i<arr.length;i++){
-           if(hs.containsKey(arr[i]*2) && hs.get(arr[i]*2)!=i)
-           {
-               return true;
-           }
-        }
+
         return false;
+    }
+
+    private int doBinarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
