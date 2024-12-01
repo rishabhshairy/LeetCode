71bb9f6dfb1071207360ebfb0e5a1e3edf7f3578
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-01 09:09:24 +0000

    [LeetCode Sync] Runtime - 7 ms (34.11%), Memory - 55.2 MB (37.39%)

--- solutions/2089-maximum-matrix-sum/solution.java
+++ solutions/2089-maximum-matrix-sum/solution.java
@@ -1,21 +1,27 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        long ans = 0;
-        int neg = 0;
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                if(matrix[i][j] < 0) {
-                    neg++;
+        long totalSum = 0;
+        long mini = Integer.MAX_VALUE;
+        int negatives = 0;
+
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+
+                if (matrix[i][j] < 0) {
+                    negatives++;
                 }
-                ans += Math.abs(matrix[i][j]);
-                if(min > Math.abs(matrix[i][j]))
-                	min = Math.abs(matrix[i][j]);
+                totalSum += Math.abs(matrix[i][j]);
+                mini = Math.min(Math.abs(matrix[i][j]), mini);
             }
         }
-        if(neg % 2 == 0)
-        	return ans;
-        else
-        	return ans - 2*min;
+
+        // now if negatives are Odd reduce sum by twice of mini
+        if ((negatives & 1) == 1) {
+            totalSum -= 2 * mini;
+        }
+        return totalSum;
     }
 }
