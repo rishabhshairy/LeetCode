9563699f05cfc4b24a38bc048965ad80322dbb28
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-02 18:05:19 +0000

    [LeetCode Sync] Runtime - 46 ms (23.68%), Memory - 56.8 MB (93.51%)

--- solutions/1147-flip-columns-for-maximum-number-of-equal-rows/solution.java
+++ solutions/1147-flip-columns-for-maximum-number-of-equal-rows/solution.java
@@ -1,22 +1,19 @@
-
- 
-    
 class Solution {
-    public int maxEqualRowsAfterFlips(int[][] mat) {
-        Map<String, Integer> patFreq = new HashMap<>();
-        
-        for (int[] row : mat) {
-            StringBuilder pattern = new StringBuilder();
-            if (row[0] == 0) {
-                for (int bit : row) pattern.append(bit);
-            } else {
-                for (int bit : row) pattern.append(bit ^ 1);
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+        Map<String, Integer> patternMap = new HashMap<>();
+
+        for (int[] row : matrix) {
+            StringBuilder val = new StringBuilder();
+            StringBuilder flippedVal = new StringBuilder();
+
+            for (int bit : row) {
+                val.append(bit);
+                flippedVal.append(1 - bit);
             }
-            patFreq.merge(pattern.toString(), 1, Integer::sum);
+
+            patternMap.put(val.toString(), patternMap.getOrDefault(val.toString(), 0) + 1);
+            patternMap.put(flippedVal.toString(), patternMap.getOrDefault(flippedVal.toString(), 0) + 1);
         }
-        
-        return Collections.max(patFreq.values());
+        return Collections.max(patternMap.values());
     }
 }
-      
-      
