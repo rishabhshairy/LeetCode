8d9002db745532a33acccae53799f30395f145d2
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-03 13:15:51 +0000

    [LeetCode Sync] Runtime - 19 ms (89.83%), Memory - 78.9 MB (83.65%)

---
+++ solutions/2232-adding-spaces-to-a-string/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static String addSpaces(String s, int[] spaces) {
+        StringBuilder result = new StringBuilder();
+        int start = 0;
+        for (int space : spaces) {
+            result.append(s, start, space).append(" ");
+            start = space;
+        }
+        result.append(s.substring(start));
+        return result.toString();
+    }
+}
