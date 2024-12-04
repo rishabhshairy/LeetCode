ef8de4984fd51c4b922e19613dff07b95f851713
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-04 17:58:18 +0000

    [LeetCode Sync] Runtime - 9 ms (33.61%), Memory - 79.6 MB (50.11%)

--- solutions/1972-rotating-the-box/solution.java
+++ solutions/1972-rotating-the-box/solution.java
@@ -1,61 +1,48 @@
-
 class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int row = boxGrid.length;
+        int col = boxGrid[0].length;
+        char[][] rotatedBox = new char[col][row];
 
-    public char[][] rotateTheBox(char[][] box) {
-        int m = box.length;
-        int n = box[0].length;
-        char[][] result = new char[n][m];
-
-        // Create the transpose of the input grid in `result`
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                result[i][j] = box[j][i];
+        // transpose matrix
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                rotatedBox[j][i] = boxGrid[i][j];
             }
         }
 
-        // Reverse each row in the transpose grid to complete the 90° rotation
-        for (int i = 0; i < n; i++) {
-            reverse(result[i]);
+        // rotate the box 90 degrees by reversing the row
+        for (char[] currRow : rotatedBox) {
+            int i = 0, j = currRow.length - 1;
+            while (i < j) {
+                char temp = currRow[i];
+                currRow[i] = currRow[j];
+                currRow[j] = temp;
+                i++;
+                j--;
+            }
         }
+        // now operate on each column
+        // start from bottom
 
-        // Apply gravity to let stones fall to the lowest possible empty cell in each column
-        for (int j = 0; j < m; j++) {
-            // Process each cell in column `j` from bottom to top
-            for (int i = n - 1; i >= 0; i--) {
-                if (result[i][j] == '.') { // Found an empty cell; check if a stone can fall into it
-                    int nextRowWithStone = -1;
+        for (int j = 0; j < row; j++) {
+            // process column from bottom to top
+            int lowestEmpty = col - 1;
+            for (int i = col - 1; i >= 0; i--) {
 
-                    // Look for a stone directly above the empty cell `result[i][j]`
-                    for (int k = i - 1; k >= 0; k--) {
-                        if (result[k][j] == '*') break; // Obstacle blocks any stones above
-                        if (result[k][j] == '#') { // Stone found with no obstacles in between
-                            nextRowWithStone = k;
-                            break;
-                        }
-                    }
+                if (rotatedBox[i][j] == '#') {
+                    rotatedBox[i][j] = '.';
+                    rotatedBox[lowestEmpty][j] = '#'; // stone fell
+                    lowestEmpty--;
+                }
 
-                    // If a stone was found above, let it fall into the empty cell `result[i][j]`
-                    if (nextRowWithStone != -1) {
-                        result[nextRowWithStone][j] = '.';
-                        result[i][j] = '#';
-                    }
+                // if obstacle is found, reduce lowestEmpty to one cell above
+                if (rotatedBox[i][j] == '*') {
+                    lowestEmpty = i - 1;
                 }
             }
         }
-        return result;
-    }
 
-    // Helper function to reverse an array
-    private void reverse(char[] row) {
-        int left = 0;
-        int right = row.length - 1;
-        while (left < right) {
-            char temp = row[left];
-            row[left] = row[right];
-            row[right] = temp;
-            left++;
-            right--;
-        }
+        return rotatedBox;
     }
 }
-
