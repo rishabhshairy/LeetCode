f38d03b68645d97b5c096daaad344f0a8fceb127
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-15 15:09:21 +0000

    [LeetCode Sync] Runtime - 6 ms (93.05%), Memory - 42.6 MB (26.61%)

--- solutions/0205-isomorphic-strings/solution.java
+++ solutions/0205-isomorphic-strings/solution.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        int[] smap = new int[256];
-        int[] tmap = new int[256];
+        		if (s.length() != t.length()) {
+			return false;
+		}
 
-        if (s.length() != t.length()) {
-            return false;
-        }
+		int[] smap = new int[256];
+		int[] tmap = new int[256];
+		int n = s.length();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (smap[s.charAt(i)] != tmap[t.charAt(i)]) {
-                return false;
-            }
-            smap[s.charAt(i)] = i + 1;
-            tmap[t.charAt(i)] = i + 1;
-        }
+		for (int i = 0; i < n; i++) {
 
-        return true;
+			if (smap[s.charAt(i)] != tmap[t.charAt(i)]) {
+				return false;
+			}
+
+			smap[s.charAt(i)] = i + 1;
+			tmap[t.charAt(i)] = i + 1;
+
+		}
+		return true;
     }
 }
