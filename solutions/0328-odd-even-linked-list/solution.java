c01bb8b430370b21595031c532942f3f84481874
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-21 11:37:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (92.82%)

--- solutions/0328-odd-even-linked-list/solution.java
+++ solutions/0328-odd-even-linked-list/solution.java
@@ -1,32 +1,30 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        
-        if(head ==null){
+        if(head == null){
             return head;
         }
         ListNode odd = head;
-        head=odd;
+        head = odd;
         ListNode even = head.next;
-        ListNode evenHead=even;
-        
-        while(even!=null && even.next!=null){
-            
-            odd.next=odd.next.next;
-            even.next=even.next.next;
-            even=even.next;
-            odd=odd.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            even = even.next;
+            odd = odd.next;
         }
-        odd.next=evenHead;
+        odd.next = evenHead;
         return head;
     }
 }
