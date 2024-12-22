fbd6093734221080f826ad12c57f3b105953927c
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-22 07:37:51 +0000

    [LeetCode Sync] Runtime - 9 ms (94.71%), Memory - 56.7 MB (62.55%)

--- solutions/0148-sort-list/solution.java
+++ solutions/0148-sort-list/solution.java
@@ -9,52 +9,58 @@
  * }
  */
 class Solution {
-    public ListNode merge(ListNode h1, ListNode h2){
-        if(h1 == null){
-            return h2;
-        }
-        if(h2 == null){
-            return h1;
-        }
-        
-        if(h1.val < h2.val){
-            h1.next = merge(h1.next, h2);
-            return h1;
-        }
-        else{
-            h2.next = merge(h1, h2.next);
-            return h2;
-        }
-        
-    }
-    
     public ListNode sortList(ListNode head) {
-        //bottom case
-        if(head == null){
+        if (head == null || head.next == null) {
             return head;
         }
-        if(head.next == null){
-            return head;
+
+        // first find middle
+        ListNode middleNode = findMiddle(head);
+
+        ListNode right = middleNode.next;
+        middleNode.next = null;
+        ListNode left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (left != null && right != null) {
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
         }
-        
-        //p1 move 1 step every time, p2 move 2 step every time, pre record node before p1
-        ListNode p1 = head;
-        ListNode p2 = head;
-        ListNode pre = head;
-        
-        while(p2 != null && p2.next != null){
-            pre = p1;
-            p1 = p1.next;
-            p2 = p2.next.next;
+
+        // append remaining list
+        if (left != null) {
+            temp.next = left;
+        } else {
+            temp.next = right;
         }
-        //change pre next to null, make two sub list(head to pre, p1 to p2)
-        pre.next = null;
-        
-        //handle those two sub list
-        ListNode h1 = sortList(head);
-        ListNode h2 = sortList(p1);
-        
-        return merge(h1, h2);
-        
+
+        return dummy.next;
+    }
+
+    private ListNode findMiddle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
