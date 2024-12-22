7da2bc679bdc60202cd485fd02af6e690d682711
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-22 10:07:27 +0000

    [LeetCode Sync] Runtime - 1 ms (99.90%), Memory - 48.4 MB (65.95%)

--- solutions/0160-intersection-of-two-linked-lists/solution.java
+++ solutions/0160-intersection-of-two-linked-lists/solution.java
@@ -11,18 +11,17 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA==null || headB==null){
+        if (headA == null || headB == null) {
             return null;
         }
-        
-        ListNode a=headA;
-        ListNode b=headB;
-        
-        while(a!=b){
-            a=a==null?headB:a.next;
-            b=b==null?headA:b.next;
+
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
         return a;
-        
     }
 }
