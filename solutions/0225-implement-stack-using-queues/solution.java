11265c147e0df8a76483edb251aee434ec70b6e7
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-25 06:38:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.6 MB (25.92%)

--- solutions/0225-implement-stack-using-queues/solution.java
+++ solutions/0225-implement-stack-using-queues/solution.java
@@ -1,37 +1,33 @@
 class MyStack {
-
-    /** Initialize your data structure here. */
     Queue<Integer> q1;
     Queue<Integer> q2;
+
     public MyStack() {
-        q1=new LinkedList<>();
-        q2=new LinkedList<>();
-        
+
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
-    
-    /** Push element x onto stack. */
+
     public void push(int x) {
-        while(!q1.isEmpty()){
+        while (!q1.isEmpty()) {
             q2.add(q1.remove());
         }
+        // add to queue
         q1.add(x);
-        while(!q2.isEmpty()){
+
+        while (!q2.isEmpty()) {
             q1.add(q2.remove());
         }
     }
-    
-    /** Removes the element on top of the stack and returns that element. */
+
     public int pop() {
-       
         return q1.remove();
     }
-    
-    /** Get the top element. */
+
     public int top() {
         return q1.peek();
     }
-    
-    /** Returns whether the stack is empty. */
+
     public boolean empty() {
         return q1.isEmpty() && q2.isEmpty();
     }
