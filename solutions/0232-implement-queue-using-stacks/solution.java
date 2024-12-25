07d1d63fe0a513132fd5d9fa9578d243381cd0a1
Rishabh Shairy <rishusinha8@gmail.com>
2024-12-25 06:46:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.1 MB (92.16%)

--- solutions/0232-implement-queue-using-stacks/solution.java
+++ solutions/0232-implement-queue-using-stacks/solution.java
@@ -1,37 +1,31 @@
 class MyQueue {
 
-    /** Initialize your data structure here. */
     Stack<Integer> s1;
     Stack<Integer> s2;
-    public MyQueue() {
-        s1=new Stack<>();
-        s2=new Stack<>();
-        
-    }
-    
-    /** Push element x to the back of queue. */
-    public void push(int x) {
-        s1.push(x);
-    }
-    
-    /** Removes the element from in front of queue and returns that element. */
-    public int pop() {
-        peek();
-        return s2.pop();
-    }
-    
-    /** Get the front element. */
-    public int peek() {
-        if(s2.empty()){
-         while(!s1.isEmpty()){
-             s2.push(s1.pop());
-         }  
-        }
 
-        return s2.peek();
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
     }
-    
-    /** Returns whether the queue is empty. */
+
+    public void push(int x) {
+        while (!s1.isEmpty()) {
+            s2.add(s1.pop());
+        }
+        s1.push(x);
+        while (!s2.empty()) {
+            s1.add(s2.pop());
+        }
+    }
+
+    public int pop() {
+        return s1.pop();
+    }
+
+    public int peek() {
+        return s1.peek();
+    }
+
     public boolean empty() {
         return s1.isEmpty() && s2.isEmpty();
     }
