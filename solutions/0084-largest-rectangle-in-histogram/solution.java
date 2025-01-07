202c1c832d90f8eb4b5a42b3e060adfe70bf0cfc
Rishabh Shairy <rishusinha8@gmail.com>
2025-01-07 05:01:48 +0000

    [LeetCode Sync] Runtime - 64 ms (70.05%), Memory - 57.6 MB (50.22%)

--- solutions/0084-largest-rectangle-in-histogram/solution.java
+++ solutions/0084-largest-rectangle-in-histogram/solution.java
@@ -1,55 +1,27 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nsl = findSmallLeft(heights);
-        int[] nsr = findSmallestRight(heights);
-        int[] width = new int[heights.length];
-        int area = Integer.MIN_VALUE;
-
-        for (int i = 0; i < heights.length; i++) {
-            width[i] = nsr[i] - nsl[i] - 1;
-            area = Math.max(heights[i] * width[i], area);
+        if (heights.length == 1) {
+            return heights[0];
         }
 
-        return area;
-    }
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        int maxArea = Integer.MIN_VALUE;
 
-    private static int[] findSmallLeft(int[] heights) {
-        int[] nsl = new int[heights.length];
-        Stack<Integer> index = new Stack<>();
-        for (int i = 0; i < heights.length; i++) {
-            if (index.isEmpty()) {
-                nsl[i] = -1;
-            } else if (!index.isEmpty() && heights[i] > heights[index.peek()]) {
-                nsl[i] = index.peek();
-            } else if (!index.isEmpty() && heights[i] <= heights[index.peek()]) {
-                while (!index.isEmpty() && heights[i] <= heights[index.peek()]) {
-                    index.pop();
-                }
-
-                nsl[i] = index.empty() ? -1 : index.peek();
+        while (index < heights.length) {
+            if (stack.isEmpty() || heights[stack.peek()] <= heights[index]) {
+                stack.push(index++);
+            } else {
+//                System.out.println(stack);
+                int top = stack.pop();
+                int currArea = heights[top] * (stack.isEmpty() ? index : index - stack.peek() - 1);
+                maxArea = Math.max(maxArea, currArea);
             }
-            index.push(i);
         }
-        return nsl;
-    }
 
-    private static int[] findSmallestRight(int[] heights) {
-        int[] nsr = new int[heights.length];
-        Stack<Integer> index = new Stack<>();
-        for (int i = heights.length - 1; i >= 0; i--) {
-            if (index.isEmpty()) {
-                nsr[i] = heights.length;
-            } else if (!index.isEmpty() && heights[i] > heights[index.peek()]) {
-                nsr[i] = index.peek();
-            } else if (!index.isEmpty() && heights[i] <= heights[index.peek()]) {
-                while (!index.isEmpty() && heights[i] <= heights[index.peek()]) {
-                    index.pop();
-                }
-
-                nsr[i] = index.empty() ? heights.length : index.peek();
-            }
-            index.push(i);
+        while (!stack.isEmpty()) {
+            maxArea = Math.max(maxArea, heights[stack.pop()] * (stack.isEmpty() ? index : index - stack.peek() - 1));
         }
-        return nsr;
+        return maxArea;
     }
 }
