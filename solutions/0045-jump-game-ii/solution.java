e1087e563d936385260c9885ca32d0c8f0b9de94
Rishabh Shairy <rishusinha8@gmail.com>
2025-01-18 14:04:56 +0000

    [LeetCode Sync] Runtime - 1 ms (99.48%), Memory - 45.7 MB (13.43%)

--- solutions/0045-jump-game-ii/solution.java
+++ solutions/0045-jump-game-ii/solution.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int jump(int[] nums) {
-                int n = nums.length;
-        int jump = 0, left = 0, right = 0;
-        while (right < n - 1) {
-            int farthest = 0;
-            for (int i = left; i <= right; i++) {
-                farthest = Math.max(farthest, i + nums[i]);
-            }
-            left = right + 1;
-            right = farthest;
-            jump++;
-        }
-        return jump;
+    public int jump(int[] arr) {
+        int end = 0, far = 0;
+		int minJumps = 0;
+
+		for (int i = 0; i < arr.length-1; i++) {
+			far = Math.max(far, arr[i] + i);
+
+			// jumped to next region
+			if (i == end) {
+				minJumps += 1;
+				end = far;
+			}
+		}
+		return minJumps;
     }
 }
