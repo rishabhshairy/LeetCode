4c764fbc4cff87e29c9ad9017ee42366a8a974f7
Rishabh Shairy <rishusinha8@gmail.com>
2025-01-21 17:17:05 +0000

    [LeetCode Sync] Runtime - 4 ms (99.63%), Memory - 45.7 MB (25.53%)

--- solutions/0680-valid-palindrome-ii/solution.java
+++ solutions/0680-valid-palindrome-ii/solution.java
@@ -1,22 +1,27 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        return solve(s, 0, s.length() - 1, false);
-    }
+        int left = 0, right = s.length() - 1;
 
-    private boolean solve(String s, int left, int right, boolean used) {
-        if (left >= right) {
-            return true;
-        }
-        if (s.charAt(left) != s.charAt(right)) {
-            if (!used) {
-                if (solve(s, left + 1, right, true)) {
-                    return true;
-                }
-                return solve(s, left, right - 1, true);
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
             } else {
-                return false;
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
             }
         }
-        return solve(s, left + 1, right - 1, used);
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left <= right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
