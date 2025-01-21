4931884cdda45c8a6d42381461bc5e7122f0345d
Rishabh Shairy <rishusinha8@gmail.com>
2025-01-21 18:12:52 +0000

    [LeetCode Sync] Runtime - 3 ms (90.07%), Memory - 43.8 MB (85.91%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.java
+++ solutions/0003-longest-substring-without-repeating-characters/solution.java
@@ -1,32 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int left = 0;
-        int right = 0;
-        int n = s.length();
-        int maxLen = Integer.MIN_VALUE;
-        int len = 0;
-        int[] hash = new int[255];
-        Arrays.fill(hash, -1);
-
-        if (n == 0) {
+        if(s.length()==0){
             return 0;
         }
+        int left = 0;
+		int right = 0;
+		int maxLen = Integer.MIN_VALUE;
 
-        while (right < n) {
+		int[] charPos = new int[255];
+		Arrays.fill(charPos, -1);
 
-            // already visited
-            if (hash[s.charAt(right)] != -1) {
-                // move left pointer one index ahead of repeating character's current position
-                if (hash[s.charAt(right)] >= left) {
-                    left = hash[s.charAt(right)] + 1;
+		while (right < s.length()) {
+
+			// critical section -- if char is found previously
+			if (charPos[s.charAt(right)] != -1) {
+				// move char ahead one postion of current repeating
+                if(charPos[s.charAt(right)] >= left) {
+                    left = charPos[s.charAt(right)] + 1;
                 }
-            }
-            len = right - left + 1;
-            maxLen = Math.max(len, maxLen);
-            hash[s.charAt(right)] = right;
-            right++;
-        }
+				
+			}
 
-        return maxLen;
+			int len = right - left + 1;
+			maxLen = Math.max(maxLen, len);
+			charPos[s.charAt(right)] = right; // marking index of each position
+			right++;
+
+		}
+		return maxLen;
     }
 }
