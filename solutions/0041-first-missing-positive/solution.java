c939135e6ef54bcbd080e46ec7fc4f47de6e0fc5
Rishabh Shairy <rishusinha8@gmail.com>
2025-01-21 05:14:59 +0000

    [LeetCode Sync] Runtime - 17 ms (13.84%), Memory - 56.3 MB (93.50%)

--- solutions/0041-first-missing-positive/solution.java
+++ solutions/0041-first-missing-positive/solution.java
@@ -1,30 +1,12 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= 0 || nums[i] > n) {
-                nums[i] = n + 1;
-            }
-
+        Arrays.sort(nums);
+        int start = 1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == start){
+                start++;
+            } 
         }
-
-        for (int i = 0; i < n; i++) {
-            int num = Math.abs(nums[i]);
-            if (num > n) {
-                continue;
-            }
-            num--;
-            if (nums[num] > 0) {
-                nums[num] = -1 * nums[num];
-            }
-
-        }
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                return i + 1;
-            }
-        }
-        return n + 1;
+        return start;
     }
 }
