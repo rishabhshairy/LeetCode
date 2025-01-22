09fc2b7c999282dbd44b2fe8eb9afba46c27e6dc
Rishabh Shairy <rishusinha8@gmail.com>
2025-01-22 04:08:31 +0000

    [LeetCode Sync] Runtime - 42 ms (56.36%), Memory - 47 MB (48.20%)

--- solutions/0416-partition-equal-subset-sum/solution.java
+++ solutions/0416-partition-equal-subset-sum/solution.java
@@ -1,39 +1,35 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
+        // step 1 check if sum is even
+		int totalSum = Arrays.stream(nums).sum();
+		if (totalSum % 2 != 0) {
+			return false;
+		}
 
-        if (totalSum % 2 == 0) {
-            int halfSum = totalSum / 2;
-            return findSubsetSum(nums, n, halfSum);
-        }
-        return false;
-    }
+		int sum = totalSum / 2;
+		int n = nums.length;
+		// now apply subset sum logic
+		boolean[][] dp = new boolean[n][sum + 1];
 
-    private static boolean findSubsetSum(int[] arr, int n, int target) {
-        boolean[][] dp = new boolean[n][target + 1];
+		// tabulation
+		for (int i = 0; i < dp.length; i++) {
+			dp[i][0] = true;
+		}
 
-        for (int i = 0; i < dp.length; i++) {
-            dp[i][0] = true;
-        }
+		if (nums[0] <= sum) {
+			dp[0][nums[0]] = true;
+		}
 
-        if (arr[0] <= target) {
-            dp[0][arr[0]] = true;
-        }
-
-        for (int index = 1; index < n; index++) {
-            for (int currTarget = 1; currTarget <= target; currTarget++) {
-                boolean notPicked = dp[index - 1][currTarget];
-                boolean picked = false;
-                if (arr[index] <= currTarget) {
-                    picked = dp[index - 1][currTarget - arr[index]];
-                }
-                dp[index][currTarget] = picked || notPicked;
-            }
-        }
-        return dp[n - 1][target];
+		for (int i = 1; i < n; i++) {
+			for (int j = 1; j <= sum; j++) {
+				boolean notPick = dp[i - 1][j];
+				boolean pick = false;
+				if (nums[i] <= j) {
+					pick = dp[i - 1][j - nums[i]];
+				}
+				dp[i][j] = pick || notPick;
+			}
+		}
+		return dp[n-1][sum];
     }
 }
