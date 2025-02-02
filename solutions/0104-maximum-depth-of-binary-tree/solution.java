4baebd1a69373e73ca5bb9156da9576c2810828e
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-02 08:04:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.2 MB (10.59%)

--- solutions/0104-maximum-depth-of-binary-tree/solution.java
+++ solutions/0104-maximum-depth-of-binary-tree/solution.java
@@ -1,41 +1,26 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null) {
+        if(root==null){
             return 0;
         }
 
-        Queue<TreeNode> nodeQueue = new ArrayDeque<>();
-        nodeQueue.offer(root);
-        int level = 0;
-
-        while (!nodeQueue.isEmpty()) {
-            int size = nodeQueue.size();
-            for (int i = 0; i < size; i++) {
-                if (nodeQueue.peek().left != null) {
-                    nodeQueue.offer(nodeQueue.peek().left);
-                }
-                if (nodeQueue.peek().right != null) {
-                    nodeQueue.offer(nodeQueue.peek().right);
-                }
-                nodeQueue.poll();
-            }
-            level++;
-        }
-        return level;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left,right);
     }
 }
