120bc68ebc555ef214faaf3e2ea01ddf08aa3b8b
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-04 18:09:08 +0000

    [LeetCode Sync] Runtime - 382 ms (10.30%), Memory - 79.7 MB (8.32%)

--- solutions/0300-longest-increasing-subsequence/solution.java
+++ solutions/0300-longest-increasing-subsequence/solution.java
@@ -1,25 +1,40 @@
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        int n = arr.length;
-        int[] next = new int[n + 1];
-        int[] curr = new int[n + 1];
+	public int lengthOfLIS(int[] nums) {
 
+		// take dp of length n
+		int n = nums.length;
+		int[][] dp = new int[n][n + 1];
 
-        // form loops
-        for (int index = n - 1; index >= 0; index--) {
-            for (int prevIndex = index - 1; prevIndex >= -1; prevIndex--) {
+		for (int[] row : dp) {
+			Arrays.fill(row, -1);
+		}
 
-                int notTake = next[prevIndex + 1];
-                int take = 0;
+		return solve(0, -1, nums, dp);
 
-                if (prevIndex == -1 || arr[index] > arr[prevIndex]) {
-                    take = 1 + next[index + 1];
+	}
 
-                }
-                curr[prevIndex + 1] = Math.max(take, notTake);
-            }
-            next = curr;
-        }
-        return next[0];
-    }
+	private int solve(int index, int prevIndex, int[] nums, int[][] dp) {
+
+		if (index == nums.length) {
+			return 0;
+		}
+
+		if (dp[index][prevIndex + 1] != -1) {
+			return dp[index][prevIndex + 1];
+		}
+
+		int notTake = solve(index + 1, prevIndex, nums, dp);
+
+		int take = 0;
+
+		// we can only take if next number is greater than previous number
+		if (prevIndex == -1 || nums[index] > nums[prevIndex]) {
+			take = 1 + solve(index + 1, index, nums, dp); // move to next index
+															// and mark prev =
+															// index
+		}
+
+		return dp[index][prevIndex + 1] = Math.max(notTake, take);
+
+	}
 }
