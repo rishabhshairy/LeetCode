36a36c1f57bf572cdac6d01af2ae6df549633026
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-04 17:18:00 +0000

    [LeetCode Sync] Runtime - 112 ms (7.26%), Memory - 45.3 MB (24.27%)

--- solutions/0322-coin-change/solution.java
+++ solutions/0322-coin-change/solution.java
@@ -1,31 +1,30 @@
 class Solution {
-    public int coinChange(int[] num, int target) {
-        // Step 1 --> declare DP array
-        int n = num.length;
-        int[][] dp = new int[n][target + 1];
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+		int[][] dp = new int[n][amount + 1];
 
-        // Step 2 --> base condition
-        // why looping from 0 --> target == dp[0][target] needs to be checked for each
-        // a[0]
-        for (int i = 0; i <= target; i++) {
-            if (i % num[0] == 0) {
-                dp[0][i] = i / num[0];
-            } else {
-                dp[0][i] = (int) Math.pow(10, 9);
-            }
-        }
-        // Step 3 --> form loops
-        for (int index = 1; index < n; index++) {
-            for (int currTarget = 0; currTarget <= target; currTarget++) {
-                int notPick = dp[index - 1][currTarget];
-                int pick = (int) Math.pow(10, 9);
-                if (num[index] <= currTarget) {
-                    pick = 1 + dp[index][currTarget - num[index]];
-                }
-                dp[index][currTarget] = Math.min(notPick, pick);
-            }
-        }
+		// base case when we have last coin in array coins[0]
 
-        return dp[n - 1][target] != (int) Math.pow(10,9) ? dp[n - 1][target] : -1;
+		for (int currentAmount = 0; currentAmount <= amount; currentAmount++) {
+			if (currentAmount % coins[0] == 0) {
+				dp[0][currentAmount] = currentAmount / coins[0];
+			} else {
+				dp[0][currentAmount] = (int) Math.pow(10, 9);
+			}
+		}
+
+		// form loops
+		for (int i = 1; i < n; i++) {
+			for (int currentAmount = 0; currentAmount <= amount; currentAmount++) {
+				int notTake = dp[i - 1][currentAmount];
+				int take = (int) Math.pow(10, 9);
+				if (coins[i] <= currentAmount) {
+					take = 1 +  dp[i][currentAmount - coins[i]];
+				}
+				dp[i][currentAmount] = Math.min(take, notTake);
+			}
+		}
+
+		return dp[n - 1][amount] != Math.pow(10, 9) ? dp[n - 1][amount] : -1;
     }
 }
