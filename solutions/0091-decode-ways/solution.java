96fb11885be87024d3f1b75bd3a7aec070b5b3e6
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-08 11:44:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.7 MB (76.68%)

--- solutions/0091-decode-ways/solution.java
+++ solutions/0091-decode-ways/solution.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int numDecodings(String s) {
-        int n = s.length();
-        Integer[] mem = new Integer[n];
-        return s.length() == 0 ? 0 : helper(s, 0, mem);
-    }
-    private int helper(String s, int index, Integer[] mem) {
-        int n = s.length();
-        if (index == s.length()) {
-            return 1;
-        }
-        if (s.charAt(index) == '0') {
-            return 0;
-        }
-        if (mem[index] != null) {
-            return mem[index];
-        }
-        int answer = helper(s, index + 1, mem);
-        if (index < n - 1 && (s.charAt(index) == '1' || (s.charAt(index) == '2' && s.charAt(index + 1) < '7'))) {
-            answer += helper(s, index + 2, mem);
-        }
-        return mem[index] = answer;
+	public int numDecodings(String s) {
+		int[] mem = new int[s.length()];
+		Arrays.fill(mem, -1);
+		return solve(0, s, mem);
+	}
 
-    }
+	private int solve(int index, String s, int[] mem) {
+		if (index == s.length()) {
+			return 1;
+		}
+		if (s.charAt(index) == '0') {
+			return 0;
+		}
+        if (mem[index]!=-1) {
+			return mem[index];
+		}
+		int pick = solve(index + 1, s, mem);
+		// most critical pair should be between 11 to 26
+		if (index < s.length() - 1 && (s.charAt(index) == '1'
+				|| s.charAt(index) == '2' && s.charAt(index + 1) < '7')) {
+			pick += solve(index + 2, s, mem);
+		}
+
+		return mem[index] = pick;
+	}
 }
