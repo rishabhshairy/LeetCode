1f81998005c08707545de7701d87473dcaf0db6c
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-08 10:30:56 +0000

    [LeetCode Sync] Runtime - 6 ms (72.38%), Memory - 41.7 MB (50.81%)

--- solutions/0647-palindromic-substrings/solution.java
+++ solutions/0647-palindromic-substrings/solution.java
@@ -1,35 +1,25 @@
 class Solution {
-    public int countSubstrings(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        int ans = 0;
+	public int countSubstrings(String s) {
+		int result = 0;
+		for (int i = 0; i < s.length(); i++) {
 
-        // base case when length 1 of string is there
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            ans++;
-        }
+			int oddCount = solve(i, i, s);
+			int evenCount = solve(i, i + 1, s);
+			result += oddCount + evenCount;
+		}
+		return result;
+	}
 
-        // base case for strings of length 2
-        for (int i = 0; i < n - 1; i++) {
-            if (s.charAt(i) == s.charAt(i + 1)) {
-                dp[i][i + 1] = true;
-                ans++;
-            }
-        }
+	private int solve(int left, int right, String s) {
 
-        // now for length 3 and above
-        for (int currLen = 3; currLen <= n; currLen++) {
-            for (int left = 0; left < n - currLen + 1; left++) {
-                int right = left + currLen - 1;
-                // System.out.println(left + " " + right);
-                // System.out.println(s.charAt(left) + " " + s.charAt(right));
-                if (s.charAt(left) == s.charAt(right) && dp[left + 1][right - 1]) {
-                    dp[left][right] = true;
-                    ans++;
-                }
-            }
-        }
-        return ans;
-    }
+		int count = 0;
+		while (left >= 0 && right < s.length()
+				&& s.charAt(left) == s.charAt(right)) {
+			left--;
+			right++;
+			count++;
+		}
+
+		return count;
+	}
 }
