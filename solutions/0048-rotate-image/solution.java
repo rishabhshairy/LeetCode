a7f74c9f5324a78e7ce741b095975856409ebf5b
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-08 15:02:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (8.09%)

--- solutions/0048-rotate-image/solution.java
+++ solutions/0048-rotate-image/solution.java
@@ -1,23 +1,21 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        // step 1 transpose the matrix
         int row = matrix.length;
-        int col = matrix[0].length;
-
-        // transpose
         for (int i = 0; i < row; i++) {
-            for (int j = i + 1; j < col; j++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
         }
 
-        // now to rotate 90 degrees reverse each column
+        // step 2 --> reverse rows for each rows now
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < row / 2; j++) {
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][row - 1 - j];
-                matrix[i][row - 1 - j] = temp;
+                matrix[i][j] = matrix[i][row - j - 1];
+                matrix[i][row - j - 1] = temp;
             }
         }
     }
