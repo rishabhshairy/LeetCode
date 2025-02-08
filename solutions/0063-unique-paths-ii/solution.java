698ee29bc8968403d548f0dfce1434ea599a5c3c
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-08 09:48:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (50.52%)

--- solutions/0063-unique-paths-ii/solution.java
+++ solutions/0063-unique-paths-ii/solution.java
@@ -1,26 +1,36 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-                int row = obstacleGrid.length;
-        int col = obstacleGrid[0].length;
-        int[][] dp = new int[row][col];
+	public int uniquePathsWithObstacles(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
 
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
+		int[][] mem = new int[m][n];
+		for (int[] row : mem) {
+			Arrays.fill(row, -1);
+		}
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    int up = i > 0 ? dp[i - 1][j] : 0;
-                    int left = j > 0 ? dp[i][j - 1] : 0;
-                    dp[i][j] = (up + left);
-                }
-            }
-        }
-        return dp[row - 1][col - 1];
-    }
+		return solve(m - 1, n - 1, grid, mem);
+	}
+
+	private int solve(int row, int col, int[][] grid, int[][] mem) {
+		// base case when obstacle is found
+		if (row >= 0 && col >= 0 && grid[row][col] == 1) {
+			return 0;
+		}
+		// when we reach 0,0 --> 1 way is found
+		if (row==0 && col==0) {
+			return 1;
+		}
+		if (row < 0 || col < 0) {
+			return 0;
+		}
+
+		if (mem[row][col] != -1) {
+			return mem[row][col];
+		}
+
+		int up = solve(row - 1, col, grid, mem);
+		int left = solve(row, col - 1, grid, mem);
+
+		return mem[row][col] = up + left;
+	}
 }
