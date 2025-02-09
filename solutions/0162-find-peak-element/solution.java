a9148dc25b9c42c8dc102b9606e598de4c6be134
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-09 15:07:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.4 MB (55.68%)

--- solutions/0162-find-peak-element/solution.java
+++ solutions/0162-find-peak-element/solution.java
@@ -1,40 +1,39 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-                int n = nums.length;
-        if (n == 1) {
-            return 0;
-        }
-        if (nums[0] > nums[1]) {
-            return 0;
-        }
-        if (nums[n - 1] > nums[n - 2]) {
-            return n - 1;
-        }
+        int n = nums.length;
 
-        // start binary search now
-        int low = 1;
-        int high = n - 2;
+		// base cases
+		if (nums.length == 1) {
+			return 0;
+		}
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+		if (nums[0] > nums[1]) {
+			return 0;
+		}
 
-            // Direct check if mid is peak
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            }
+		if (nums[n - 1] > nums[n - 2]) {
+			return n-1;
+		}
 
-            // now do the elimination of search space
-            if (nums[mid] > nums[mid - 1]) {
-                low = mid + 1;
-            } else if (nums[mid] > nums[mid + 1]) {
-                high = mid - 1;
-            } else {
-                // this is extra check for multiple peak
-                // we can go either left or right
-                low = mid + 1;
-            }
+		// if above conditions do not meet do BS on 1 - n-2
+		int left = 1;
+		int right = n - 2;
 
-        }
-        return -1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			// check direct mid
+			if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+				return mid;
+			}
+
+			// redcue search space
+			if (nums[mid] > nums[mid - 1]) { // 1 4 5 mid is 5
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+		return left;
     }
 }
