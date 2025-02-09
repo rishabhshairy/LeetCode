a3b7a78bad0955db89b51d2966c874da2894a2ea
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-09 08:45:27 +0000

    [LeetCode Sync] Runtime - 33 ms (35.96%), Memory - 69 MB (5.27%)

--- solutions/0128-longest-consecutive-sequence/solution.java
+++ solutions/0128-longest-consecutive-sequence/solution.java
@@ -1,25 +1,29 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
-        Set<Integer> numSet = new TreeSet();
-        for (int num : nums) {
-            numSet.add(num);
-        }
-        int longest = 0;
-        int count = 1;
-        List<Integer> list = new ArrayList<Integer>(numSet);
-        System.out.println(list);
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) + 1 == list.get(i + 1)) {
-                count++;
-            } else {
-                longest = Math.max(longest, count);
-                count = 1;
-            }
-        }
-        longest = Math.max(longest, count);
-        return longest;
-    }
+	public int longestConsecutive(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		Set<Integer> numSet = new LinkedHashSet<>();
+		int count = 1;
+
+		for (int num : nums) {
+			numSet.add(num);
+		}
+
+		for (Integer num : numSet) {
+			if (!numSet.contains(num - 1)) {
+				int next = num;
+				int currCount = 1;
+
+				// check if set contains x --> x+n
+				while (numSet.contains(next + 1)) {
+					next++;
+					currCount++;
+				}
+				count = Math.max(count, currCount);
+			}
+		}
+		return count;
+	}
 }
