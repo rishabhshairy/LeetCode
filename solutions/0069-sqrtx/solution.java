8658239d4e6a537f9a332051af0e268ac20148fc
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-15 12:51:50 +0000

    [LeetCode Sync] Runtime - 1 ms (86.03%), Memory - 40.9 MB (68.80%)

--- solutions/0069-sqrtx/solution.java
+++ solutions/0069-sqrtx/solution.java
@@ -1,21 +1,24 @@
 class Solution {
-        public int mySqrt(int x) {
-        if (x <= 1) {
+    public int mySqrt(int x) {
+
+        if(x <= 1){
             return x;
         }
 
-        int left = 1;
-        int right = Integer.MAX_VALUE;
-        while (true) {
-            int mid = left + (right - left) / 2;
-            if (mid > x / mid) {
-                right = mid - 1;
+        long left = 1;
+        long right = x;
+        long ans = 1;
+
+        while(left<=right) {
+            long mid = left + (right - left) / 2;
+            
+            if(mid * mid <= x){
+                ans = mid;
+                left = mid+1;
             } else {
-                if (mid + 1 > x / (mid + 1)) {
-                    return mid;
-                }
-                left = mid + 1;
+                right = mid - 1;
             }
         }
-        }
+        return (int) ans;
+    }
 }
