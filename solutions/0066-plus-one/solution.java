1877422202dccd0214226e41cd66aa134a73f484
Rishabh Shairy <rishusinha8@gmail.com>
2025-02-15 11:33:39 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.8 MB (66.16%)

--- solutions/0066-plus-one/solution.java
+++ solutions/0066-plus-one/solution.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] != 9) {
+        for(int i=n-1;i>=0;i--){
+            // add 1 and return if last digit is < 9
+            if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
-            } else {
-                digits[i] = 0;
             }
+            // make current digit 0 if == 9
+            digits[i] = 0;
         }
-        
-        int[] newArr = Arrays.copyOf(digits, digits.length + 1);
-        newArr[0] = 1;
-        return newArr;
+
+        // add 1 to 0th position
+        digits = new int[n+1];
+        digits[0] = 1;
+        return digits;
     }
 }
